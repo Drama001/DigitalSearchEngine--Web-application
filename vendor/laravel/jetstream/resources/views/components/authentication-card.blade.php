94adb38c394c7581201137a659788fd0e05191b7
Divyasree Ramagiri <drama001@odu.edu>
2022-10-04 11:43:10 -0400

    initial_commit

---
+++ vendor/laravel/jetstream/resources/views/components/authentication-card.blade.php
@@ -0,0 +1,8 @@
+<div class="min-h-screen flex-col sm:justify-center items-center pt-6 sm:pt-0" style=" margin-top: 100px; margin-left:500px;">
+    <div>
+        {{ $logo }}
+    </div>
+    <div class="w-full sm:max-w-lg px-6 py-4 bg-gray-200 shadow-md overflow-hidden">
+        {{ $slot }}
+    </div>
+</div>
